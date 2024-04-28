8bd8b7bb796e03fffb61fec4342077ebda71e627
cxinu <72374438+cxinu@users.noreply.github.com>
2024-04-28 23:30:22 +0530

    🩹 Use client

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,14 @@
+'use client'
+
 import { signIn } from 'next-auth/react'
 import { NextPage } from 'next'
+import { Button } from '@/components/ui/button'
 
 const LoginPage: NextPage = () => {
   return (
     <div>
       <p>This is login page - public route</p>
-      <button onClick={() => signIn('github')}>Sign in with github</button>
+      <Button onClick={() => signIn('github')}>Sign in with github</Button>
     </div>
   )
 }
