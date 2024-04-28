fe31c1d1252238c8674ed42d828178f8026e8537
cxinu <72374438+cxinu@users.noreply.github.com>
2024-04-28 11:24:54 +0530

    📝 Add new files for documentation page and header component

---
+++ app/docs/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Header from '@/components/docs/header'
+import Main from '@/components/docs/main'
+
+export default function Docs() {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  )
+}
